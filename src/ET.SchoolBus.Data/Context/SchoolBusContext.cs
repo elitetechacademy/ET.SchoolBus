904f3a12b837a547e0401aa5cac06557c83a1a84
elitetechacademy <info.elitetechacademy@gmail.com>
2024-11-02 23:40:31 +0300

    Appsettings connectionstring set

--- src/ET.SchoolBus.Data/Context/SchoolBusContext.cs
+++ src/ET.SchoolBus.Data/Context/SchoolBusContext.cs
@@ -1,4 +1,5 @@
 using System;
+using ET.SchoolBus.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ET.SchoolBus.Data.Context;
@@ -10,5 +11,14 @@ public class SchoolBusContext : DbContext
         
     }
 
-    
+    public DbSet<Brand> Brands { get; set; }
+    public DbSet<Driver> Drivers { get; set; }
+    public DbSet<Hostes> Hosteses { get; set; }
+    public DbSet<Model> Models { get; set; }
+    public DbSet<Parent> Parents { get; set; }
+    public DbSet<Profession> Professions { get; set; }
+    public DbSet<School> Schools { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<StudentParent> StudentParents { get; set; }
+    public DbSet<Vehicle> Vehicles { get; set; }
 }
