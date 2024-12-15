1bd216996e8cbddcbf21e9d596551d6c95abb4d4
elitetechacademy <info.elitetechacademy@gmail.com>
2024-12-15 20:31:01 +0300

    ModelDto için BrandId özelliği eklendi.

--- src/ET.SchoolBus.Application/DTOs/Response/ModelDto.cs
+++ src/ET.SchoolBus.Application/DTOs/Response/ModelDto.cs
@@ -4,6 +4,7 @@ namespace ET.SchoolBus.Application.DTOs.Response;
 
 public class ModelDto
 {
+    public int BrandId { get; set; }
     public int ModelId { get; set; }
     public string ModelName { get; set; }
 }
